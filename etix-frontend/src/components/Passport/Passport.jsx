6752c4ff9380574dd6abc6a6afceec659075fe6f
Matt <43501106+mattlab7@users.noreply.github.com>
2021-09-18 00:15:38 +0800

    Passport (Login/Register)

--- etix-frontend/src/components/Passport/Passport.jsx
+++ etix-frontend/src/components/Passport/Passport.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/styles';
-import { Link, Container, Grid, Box, Typography } from '@material-ui/core'
+import { Grid, Box, Typography, TextField, Button } from '@material-ui/core'
+
 
 const useStyles = makeStyles((theme) => ({
     whole: {
@@ -14,20 +15,79 @@ const useStyles = makeStyles((theme) => ({
     inside: {
       paddingTop: '5%',
       paddingBottom: '5%'
+    },
+    border: {
+        borderLeft: '5px solid black',
+        position: 'absolute',
+        left: '50%',
+        marginTop: '30px',
+        marginLeft: '15px',
+        marginRight: '30px',
+        height: '300px'
+    },
+    login: {
+        paddingRight: '5%',
+        marginRight: '15%'
+    },
+    register: {
+        paddingLeft: '5%',
+        marginLeft: '15%'
+    },
+    space: {
+        width: '25px'
     }
-  }));
+}));
 
 function Passport() {
     const classes = useStyles();
     return (
         <Box className={classes.whole}>
-            <Grid container direction="column" justifyContent="center" alignItems="center" spacing={2} className={classes.inside}>
+            <Grid container direction="column" justifyContent="center" alignItems="center" spacing={4} className={classes.inside}>
                 <Grid item>
                     <Typography variant="h4">welcome, or welcome back!</Typography>
                 </Grid>
                 <Grid item>
-                    <Grid container direction="row" spacing={8}>
-                        
+                    <Grid container direction="row" spacing={10}>
+                        <Grid item>
+                            <Grid container direction="column" spacing={2} className={classes.login}>
+                                <Grid item>
+                                    <TextField label="Email" variant="filled"/>
+                                </Grid>
+                                <Grid item>
+                                    <TextField label="Password" variant="filled"/>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="contained">Login</Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="filled">Forgot your password?</Button>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item className={classes.space}></Grid>
+                        <Grid item className={classes.space}></Grid>
+                        <Grid item className={classes.border}></Grid>
+                        <Grid item className={classes.space}></Grid>
+                        <Grid item className={classes.space}></Grid>
+                        <Grid item>
+                            <Grid container direction="column" spacing={2} className={classes.register}>
+                                <Grid item>
+                                    <TextField label="Email" variant="filled"/>
+                                </Grid>
+                                <Grid item>
+                                    <TextField label="Password" variant="filled"/>
+                                </Grid>
+                                <Grid item>
+                                    <TextField label="Username" variant="filled"/>
+                                </Grid>
+                                <Grid item>
+                                    <TextField label="Phone Number" variant="filled"/>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="contained">Login</Button>
+                                </Grid>
+                            </Grid>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>
