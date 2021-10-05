d51dcd9335b432acb8541076cdb562b9e36db044
Matt <43501106+mattlab7@users.noreply.github.com>
2021-10-05 15:38:26 +0800

    Revert "Merge branch 'eTix-Main' into Development"
    This reverts commit 651c35310bc6089bb4e59a915638fc9ef3848d01, reversing
    changes made to 6f0408cc5740e75443faedba781f8559ea007cd2.

--- etix-frontend/src/App.js
+++ etix-frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Header from '../src/components/Homepage/Header/header'
@@ -87,21 +86,6 @@ function App() {
       <div>
         <Router />
       </div>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
