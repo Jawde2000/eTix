1222623bdc215eba1d0a75728cfcdb04fe3b470f
koee <1191202709@student.mmu.edu.my>
2021-11-30 10:09:29 +0800

    send email after verify, reset after click logo

--- etix-admin/src/components/NavBar/NavBar.jsx
+++ etix-admin/src/components/NavBar/NavBar.jsx
@@ -5,7 +5,11 @@ import React, { useState } from 'react';
 import etixLogo from '../globalAssets/eTixLogo.png'
 import Clock from 'react-live-clock'
 import moment from 'moment';
-
+import { HELP_LIST_RESET, HELP_DELETE_RESET, HELP_DETAIL_RESET, HELP_SAVE_RESET, HELP_SEND_RESPONSE_RESET  } from '../../constants/helpConstants';
+import { USER_UPDATE_RESET, USER_LIST_RESET, USER_DETAIL_RESET, USER_DELETE_RESET, USER_CUSTOMER_REGISTER_RESET, USER_REGISTER_RESET, USER_VENDOR_REGISTER_RESET, USER_VENDOR_UPDATE_RESET, USER_CUSTOMER_UPDATE_RESET } from '../../constants/userConstants';
+import {SERVICE_LIST_RESET, SERVICE_DETAIL_RESET, SERVICE_ADD_RESET, SERVICE_DELETE_RESET, SERVICE_SAVE_RESET, VENDOR_DETAIL_RESET, LOCATION_DETAIL_RESET} from '../../constants/serviceConstants'
+import {SERVICE_LIST_DATAGENERATION_RESET, PAYMENT_LIST_RESET} from '../../constants/salesConstants';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -45,6 +49,37 @@ function NavBar() {
   const [value] = useState("/");
   var dateAndTime= moment().format("DD/MM/YYYY");
 
+  const dispatch = useDispatch();
+
+  const handleReset = () => {
+    dispatch({type: HELP_LIST_RESET});
+    dispatch({type: HELP_DELETE_RESET});
+    dispatch({type: HELP_DETAIL_RESET});
+    dispatch({type: HELP_SAVE_RESET});
+    dispatch({type: HELP_SEND_RESPONSE_RESET});
+    
+    dispatch({type: USER_UPDATE_RESET});
+    dispatch({type: USER_LIST_RESET});
+    dispatch({type: USER_DETAIL_RESET});
+    dispatch({type: USER_DELETE_RESET});
+    dispatch({type: USER_CUSTOMER_REGISTER_RESET});
+    dispatch({type: USER_REGISTER_RESET});
+    dispatch({type: USER_VENDOR_REGISTER_RESET});
+    dispatch({type: USER_VENDOR_UPDATE_RESET});
+    dispatch({type: USER_CUSTOMER_UPDATE_RESET});
+
+    dispatch({type: SERVICE_LIST_RESET})
+    dispatch({type: SERVICE_DETAIL_RESET})
+    dispatch({type: SERVICE_ADD_RESET})
+    dispatch({type: SERVICE_DELETE_RESET})
+    dispatch({type: SERVICE_SAVE_RESET})
+    dispatch({type: VENDOR_DETAIL_RESET})
+    dispatch({type: LOCATION_DETAIL_RESET})
+
+    dispatch({type: SERVICE_LIST_DATAGENERATION_RESET})
+    dispatch({type: PAYMENT_LIST_RESET})
+  }
+
   return (
           <AppBar className={defaultStyle.customizeAppbar} position="relative">
             <Grid xs={12} container >
@@ -52,7 +87,7 @@ function NavBar() {
            
               <Grid xs={12} container justify="center" direction="row" alignItems="center">
               <Grid item>
-              <Link to={value}           
+              <Link to={value} onClick={handleReset}          
                 style={{ textDecorationLine: 'none', display: "flex",}}>        
                   <img src={etixLogo} alt="etixLogo" className={defaultStyle.resizePic}/>
                   <Typography className={defaultStyle.resizeLogo} variant="h2" sx={{ flexGrow: 1 }} style={{fontFamily: ['rubik', 'sans-serif'].join(',')}}>
